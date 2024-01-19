77452b46ad05472305dc52427cf2caf493aa8080
Alif Ahsanil Satria <alif.ahsanil@BLUEBIRDGROUP.COM>
2024-01-19 13:09:33 +0700

    add retweet endpoint

---
+++ tweet/repository/db/upsert_retweet.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/alifahsanilsatria/twitter-clone/domain"
+	"github.com/sirupsen/logrus"
+)
+
+func (repo *tweetRepository) UpsertRetweet(ctx context.Context, param domain.UpsertRetweetParam) (domain.UpsertRetweetResult, error) {
+	logData := logrus.Fields{
+		"method":     "tweetRepository.UpsertRetweet",
+		"request_id": ctx.Value("request_id"),
+		"param":      fmt.Sprintf("%+v", param),
+	}
+
+	queryUpsertRetweet := `
+		insert into retweet
+		(user_id, tweet_id, is_deleted, created_at)
+		values
+		($1, $2, false, $3)
+		on conflict (user_id, tweet_id)
+		do update retweet
+		set is_deleted = false,
+		updated_at = $3
+		where user_id = $1
+		and tweet_id = $2
+	`
+
+	argsQueryUpsertRetweet := []interface{}{
+		param.UserId,
+		param.TweetId,
+		time.Now(),
+	}
+
+	logData["query_upsert_retweet"] = queryUpsertRetweet
+	logData["args_query_upsert_retwet"] = fmt.Sprintf("%+v", argsQueryUpsertRetweet)
+
+	errUpsertQuery := repo.db.QueryRowContext(ctx, queryUpsertRetweet, argsQueryUpsertRetweet...).Err()
+	if errUpsertQuery != nil {
+		logData["error_query_upsert_tweet"] = errUpsertQuery.Error()
+		repo.logger.
+			WithFields(logData).
+			WithError(errUpsertQuery).
+			Errorln("error on upsert query")
+		return domain.UpsertRetweetResult{}, errUpsertQuery
+	}
+
+	return domain.UpsertRetweetResult{}, nil
+}
