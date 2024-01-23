db66cb772227f1163e6149eb6eca8bea667277b5
Alif Ahsanil Satria <alif.ahsanil@BLUEBIRDGROUP.COM>
2024-01-23 22:10:32 +0700

    add list of following endpoint

---
+++ user/usecase/get_list_of_following_usecase.go
@@ -0,0 +1,74 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/alifahsanilsatria/twitter-clone/domain"
+	"github.com/sirupsen/logrus"
+)
+
+func (uc *userUsecase) GetListOfFollowingUsecase(ctx context.Context, param domain.GetListOfFollowingUsecaseParam) (domain.GetListOfFollowingUsecaseResult, error) {
+	logData := logrus.Fields{
+		"method":     "userUsecase.GetListOfFollowingUsecase",
+		"request_id": ctx.Value("request_id"),
+		"param":      fmt.Sprintf("%+v", param),
+	}
+
+	getUserSessionByTokenParam := domain.GetUserSessionByTokenParam{
+		Token: param.Token,
+	}
+
+	logData["get_user_session_by_token_param"] = fmt.Sprintf("%+v", getUserSessionByTokenParam)
+
+	userSession, errGetUserSession := uc.userSessionRepository.GetUserSessionByToken(ctx, getUserSessionByTokenParam)
+	if errGetUserSession != nil {
+		logData["error_get_user_session"] = errGetUserSession.Error()
+		uc.logger.
+			WithFields(logData).
+			WithError(errGetUserSession).
+			Errorln("error on GetUserSessionByToken")
+		return domain.GetListOfFollowingUsecaseResult{}, errGetUserSession
+	}
+
+	logData["get_user_session_by_token_result"] = fmt.Sprintf("%+v", userSession)
+
+	if userSession.UserId == 0 {
+		return domain.GetListOfFollowingUsecaseResult{}, errors.New("invalid or expired token")
+	}
+
+	getListOfFollowingRepoParam := domain.GetListOfFollowingRepoParam{
+		UserId: param.UserId,
+	}
+
+	logData["get_list_of_following_param"] = fmt.Sprintf("%+v", getListOfFollowingRepoParam)
+
+	getListOfFollowingRepoResult, errGetListOfFollowingRepo := uc.userRepository.GetListOfFollowingRepo(ctx, getListOfFollowingRepoParam)
+	if errGetListOfFollowingRepo != nil {
+		logData["error_get_list_of_following"] = errGetListOfFollowingRepo.Error()
+		uc.logger.
+			WithFields(logData).
+			WithError(errGetListOfFollowingRepo).
+			Errorln("error on GetListOfFollowingRepo")
+		return domain.GetListOfFollowingUsecaseResult{}, errGetListOfFollowingRepo
+	}
+
+	getListOfFollowingUsecaseResult := domain.GetListOfFollowingUsecaseResult{
+		Users: make([]domain.GetListOfFollowingUsecaseResult_User, len(getListOfFollowingRepoResult.Users)),
+	}
+
+	for idx, data := range getListOfFollowingRepoResult.Users {
+		getListOfFollowingUsecaseResult.Users[idx] = domain.GetListOfFollowingUsecaseResult_User{
+			UserId:       data.UserId,
+			Username:     data.Username,
+			CompleteName: data.CompleteName,
+		}
+	}
+
+	uc.logger.
+		WithFields(logData).
+		Infoln("success get list of following usecase")
+
+	return getListOfFollowingUsecaseResult, nil
+}
